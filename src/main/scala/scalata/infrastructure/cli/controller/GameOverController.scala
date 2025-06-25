031b43a13020f3484d8f2992ecc10f07f15eb451
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2025-06-25 12:29:11 +0200

    feat(gameloop): Add Game over case

--- src/main/scala/scalata/infrastructure/cli/controller/GameOverController.scala
+++ src/main/scala/scalata/infrastructure/cli/controller/GameOverController.scala
@@ -1,8 +1,8 @@
 package scalata.infrastructure.cli.controller
 
 import scalata.domain.entities.Player
-import scalata.domain.util.{GameControllerState, GameResult}
+import scalata.domain.util.{GameControllerState, GameError, GameResult}
 
 class GameOverController extends Controller:
   override def start(): GameResult[(GameControllerState, Option[Player])] =
-    GameResult.success(GameControllerState.GameOver, None)
+    GameResult.Error(GameError.GameOver(), "Game Over")
