49f5efc6f346171371616bca9290ab107781d139
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2025-07-07 12:23:39 +0200

    feat(entity): Enemy Attack Use Case implement

--- src/main/scala/scalata/application/usecases/enemyusecase/EnemyAttackUseCase.scala
+++ src/main/scala/scalata/application/usecases/enemyusecase/EnemyAttackUseCase.scala
@@ -1,10 +1,24 @@
 package scalata.application.usecases.enemyusecase
 
 import scalata.application.usecases.CreatureUseCase
+import scalata.domain.entities.Player
 import scalata.domain.util.{Direction, GameResult}
 import scalata.domain.world.GameSession
 
-class EnemyAttackUseCase extends CreatureUseCase[GameResult[GameSession], Direction]:
-  override def execute(param: Direction, gameSession: GameSession): GameResult[GameSession] =
-    GameResult.success(gameSession)
+class EnemyAttackUseCase extends CreatureUseCase[Player, Direction]:
+  override def execute(_param: Direction, gameSession: GameSession): Player =
+    val currentRoom = gameSession.getWorld
+      .getRoom(gameSession.getGameState.currentRoom)
+      .getOrElse(
+        throw IllegalStateException("Room Not defined in Interact use case")
+      )
+    val player = gameSession.getWorld.getPlayer
+
+    currentRoom.getAliveEnemies
+      .filter: e =>
+        e.position
+          .neighboursFiltered(currentRoom.isInside)
+          .contains(player.position)
+      .foldLeft(player): (p, e) =>
+        e.attack(p)
 
