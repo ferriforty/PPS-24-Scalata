802d28a4d5e59a5592c0e114d30b4044617ce01f
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2025-07-16 14:40:19 +0200

    test(gameloop): Non-functional tests for wrong inputs

--- src/test/scala/scalata/infrastructure/cli/controller/GameControllerTest.scala
+++ src/test/scala/scalata/infrastructure/cli/controller/GameControllerTest.scala
@@ -3,6 +3,7 @@ package scalata.infrastructure.cli.controller
 import cats.effect.IO
 import cats.syntax.all.*
 import cats.effect.unsafe.implicits.global
+import org.scalatest.Tag
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import scalata.application.services.GameBuilder
@@ -35,3 +36,15 @@ class GameControllerTest extends AnyFlatSpec with Matchers:
     intercept[IllegalStateException]:
       GameController(GameRunView[IO](testView).ask)
         .start(gameBuilder = GameBuilder(None))
+
+  "The game" should
+    "handle invalid inputs (e.g., wrong command, syntax error) without crashing or altering game state." taggedAs 
+    Tag("Non-Functional") in:
+    
+    val invalidInputs = List("!", "move 999", "lojump", "use ", "equip spear!", "#@!drop", "")
+    val gameBuilder =  GameBuilder(Some(PlayerFactory().create(PlayerClasses.Mage)))
+
+    invalidInputs.foreach: input =>
+      noException should be thrownBy GameController(GameRunView[IO](TestView(input)).ask)
+        .start(gameBuilder = gameBuilder)
+
