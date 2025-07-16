9013f9f76f72f8ad616d12f971e63531a5f257ae
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2025-07-16 18:56:42 +0200

    docs(player): Add scaladoc to player usecases

--- src/main/scala/scalata/application/usecases/playerusecases/PlayerAttackUseCase.scala
+++ src/main/scala/scalata/application/usecases/playerusecases/PlayerAttackUseCase.scala
@@ -5,10 +5,40 @@ import scalata.domain.util.Geometry.Point2D
 import scalata.domain.util.{Direction, GameResult}
 import scalata.domain.world.GameSession
 
+
+/** Executes a **player attack** in the chosen direction.
+ *
+ * <h4>Algorithm</h4>
+ * <ul>
+ * <li>Compute a *reach vector* whose length equals the player’s
+ * <code>reach</code> stat.</li>
+ * <li>Build the inclusive line of points from the player’s position to the
+ * target tile (<code>rangeTo</code>).</li>
+ * <li>For every enemy in the current room:
+ * <ul>
+ * <li>If the enemy is <i>alive</i> **and** its position lies on the
+ * line, apply <code>player.attack</code>.</li>
+ * </ul>
+ * </li>
+ * <li>Return an updated <code>GameSession</code> wrapped in
+ * <code>GameResult.success</code>.</li>
+ * </ul>
+ *
+ * <b>Failure modes</b>
+ * If the room referenced by <code>GameState.currentRoom</code> is missing, an
+ * <code>IllegalStateException</code> is thrown (world corruption guard).
+ */
 class PlayerAttackUseCase
     extends CreatureUseCase[GameResult[
       GameSession
     ], Direction]:
+
+  /** Perform one attack action.
+   *
+   * @param direction   direction chosen by the player
+   * @param gameSession immutable snapshot before the attack
+   * @return updated snapshot wrapped in a successful <code>GameResult</code>
+   */
   override def execute(
       direction: Direction,
       gameSession: GameSession
--- src/main/scala/scalata/application/usecases/playerusecases/PlayerInventoryUseCase.scala
+++ src/main/scala/scalata/application/usecases/playerusecases/PlayerInventoryUseCase.scala
@@ -5,10 +5,31 @@ import scalata.domain.entities.components.Usable
 import scalata.domain.util.{GameError, GameResult}
 import scalata.domain.world.GameSession
 
+/** Handles the **“use item”** command.
+ *
+ * <ul>
+ * <li>Looks up the item by <code>itemName</code> in the player’s inventory.</li>
+ * <li>Delegates to the <code>Usable</code> type-class to apply the effect
+ * (<i>Potion</i>, <i>Weapon</i>, <i>Dust</i>, …).</li>
+ * <li>Updates the player inside the <code>World</code> and returns a
+ * <code>GameResult.success</code> wrapping the new
+ * <code>GameSession</code>.</li>
+ * <li>If the player does not own the item, returns
+ * <code>GameResult.Error(GameError.ItemNotOwned)</code>.</li>
+ * </ul>
+ */
 class PlayerInventoryUseCase
     extends CreatureUseCase[GameResult[
       GameSession
     ], String]:
+
+  /** Apply the effect of the requested item.
+   *
+   * @param itemName    user-supplied identifier (case-insensitive, spaces stripped)
+   * @param gameSession immutable snapshot before execution
+   * @return updated session in <code>GameResult.success</code>  
+   *         or <code>ItemNotOwned</code> error
+   */
   override def execute(
       itemName: String,
       gameSession: GameSession
